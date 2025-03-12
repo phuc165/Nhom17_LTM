4e7c9a5a3f51a2c4c5bbf43cd03f18601b56c38d
hoang phan <hoangphan26868@gmail.com>
2025-03-12 19:54:11 +0700

    room

---
+++ Server/Room.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Room
+    {
+        public List<TcpClient> Players { get; set; } = new List<TcpClient>();
+        public Common.CellState[,] Board { get; set; } = new Common.CellState[Common.BOARD_SIZE, Common.BOARD_SIZE];
+        public Common.GameStatus GameStatus { get; set; } = Common.GameStatus.Waiting;
+        public int CurrentPlayerIndex { get; set; } = 0;
+        public int RoomId { get; set; }
+
+        public Room(int id)
+        {
+            RoomId = id;
+            InitializeBoard();
+        }
+
+        public void InitializeBoard()
+        {
+            for (int i = 0; i < Common.BOARD_SIZE; i++)
+            {
+                for (int j = 0; j < Common.BOARD_SIZE; j++)
+                {
+                    Board[i, j] = Common.CellState.Empty;
+                }
+            }
+        }
+    }
+}
