eb113242b0ff3d72b6f3f62782a90b4b66aae109
motbabonkhong <162330654+motbabonkhong@users.noreply.github.com>
2025-03-16 19:34:30 +0700

    common server

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+
 namespace Server
 {
-   
+    public class Common
+    {
+        public const int BOARD_SIZE = 15;
+        public const int CELL_SIZE = 40;
+        public const int WINNING_COUNT = 5;
+
+        public enum CellState { Empty, X, O }
+        public enum GameStatus { Waiting, Playing, GameOver }
+
+        public static string FormatMessage(string command, string data)
+        {
+            return $"{command}|{data}";
+        }
+
+        public static void ParseMessage(string message, out string command, out string data)
+        {
+            string[] parts = message.Split('|');
+            command = parts[0];
+            data = parts.Length > 1 ? parts[1] : "";
+        }
+    }
 }
