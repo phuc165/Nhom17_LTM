ae783f00af050b984ee768d1a419b6e3e577f981
Qua18 <nnquaok22@gmail.com>
2025-03-17 20:38:59 +0700

    Update Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -31,4 +31,57 @@ namespace Server
             data = parts.Length > 1 ? parts[1] : "";
         }
     }
+    public class CaroServer : Form
+{
+    private TcpListener server;
+    private List<Room> rooms = new List<Room>();
+    private Button startButton;
+    private Label statusLabel;
+
+    public CaroServer()
+    {
+        InitializeComponents();
+    }
+
+    private void InitializeComponents()
+    {
+        this.Text = "Caro Game Server";
+        this.Size = new Size(400, 300);
+        this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        this.MaximizeBox = false;
+
+        statusLabel = new Label
+        {
+            Text = "Server Status: Not Running",
+            Location = new Point(20, 20),
+            AutoSize = true
+        };
+
+        startButton = new Button
+        {
+            Text = "Start Server",
+            Location = new Point(20, 50),
+            Size = new Size(100, 30)
+        };
+        startButton.Click += StartServer_Click;
+
+        this.Controls.Add(statusLabel);
+        this.Controls.Add(startButton);
+
+        this.FormClosing += (s, e) => StopServer();
+    }
+
+    private void StartServer_Click(object sender, EventArgs e)
+    {
+        if (server == null)
+        {
+            StartServer();
+            startButton.Text = "Stop Server";
+        }
+        else
+        {
+            StopServer();
+            startButton.Text = "Start Server";
+        }
+    }
 }
